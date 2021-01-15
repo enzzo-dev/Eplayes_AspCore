b870b80ee0af0aa1157251bf324802f07d57e5ad
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2021-01-15 12:20:04 -0300

    Tudo arrumado

--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -5,7 +5,7 @@ using Eplayes_AspCore.Controllers;
 
 namespace Eplayes_AspCore.Models
 {
-    public class Jogador : EplayersBase , IJogador , EquipeController
+    public class Jogador
     {
         public int IdJogador { get; set; }
 
@@ -15,73 +15,5 @@ namespace Eplayes_AspCore.Models
         
         private const string PATH = "Database/Jogador.csv";
 
-         public Jogador()
-        {
-            CreateFolderAndFile(PATH);
-        }
-
-        public string Prepare(Jogador j)
-        {
-            return $"{j.IdJogador};{j.Nome};{j.IdEquipe}";
-        }
-        
-        public void Create(Jogador j)
-        {
-            string[] linhas = { Prepare(j) };
-
-            //Escrevemos linhas no arquivo csv
-            File.AppendAllLines(PATH, linhas);
-        }
-
-        public List<Jogador> ReadAll()
-        {
-
-            List<Jogador> jogadores = new List<Jogador>();
-            string[] linhas = File.ReadAllLines(PATH);
-
-            foreach (string item in linhas)
-            {
-                string[] linha = item.Split(";");
-
-                Jogador novoJogador = new Jogador();
-
-                novoJogador.IdJogador = int.Parse( linha[0] );
-                novoJogador.Nome     = linha[1];
-                novoJogador.IdEquipe   = int.Parse( linha[2] ); 
-
-                jogadores.Add(novoJogador);  
-            }
-
-           return jogadores;
-
-        }
-
-
-        public void Update(Jogador jogadorAlterado)
-        {
-            List<string> linhas = ReadAllLinesCSV(PATH); 
-
-            //Removemos as linhas com o código comparado.
-            linhas.RemoveAll(x => x.Split(";")[0] == jogadorAlterado.IdJogador.ToString());
-
-            linhas.Add( Prepare(jogadorAlterado ) );
-
-            RewriteCSV(PATH, linhas);
-
-        }
-
-
-        public void Delete(int id)
-        {
-            List<string> linhas = ReadAllLinesCSV(PATH); 
-
-            //Removemos as linhas com o código comparado.
-            linhas.RemoveAll(x => x.Split(";")[0] == id.ToString());
-
-            
-
-            RewriteCSV(PATH, linhas);
-        }
-  
     }
 }
